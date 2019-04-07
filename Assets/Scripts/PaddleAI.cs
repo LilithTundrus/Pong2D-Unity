2e809cbe5f3d61952ac9bdbc7b830073e5c398c8
Lilith Tundrus <echofox123@gmail.com>
2019-04-07 17:14:04 -0400

    Added basic paddle AI code

---
+++ Assets/Scripts/PaddleAI.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleAI : MonoBehaviour {
+    public float speed = 10.0f;
+    public float boundY = 2.25f;
+    private Rigidbody2D rb2d;
+    private GameObject ball;
+
+    // Start is called before the first frame update
+    void Start() {
+        rb2d = GetComponent<Rigidbody2D>();
+        ball = GameObject.Find("Ball");
+    }
+
+    void Update() {
+        Transform Ball;
+        Transform Ai;
+        Ball = GameObject.FindGameObjectWithTag("Ball").transform;
+        Ai = this.transform;
+
+        var pos = transform.position;
+        var vel = rb2d.velocity;
+
+
+        if (Ball.position.y > 0) {
+            Debug.Log("Ball Pos " + Ball.position.y);
+            Ai.position = new Vector3(Ai.position.x, Ball.position.y -0.6f, 0);
+        }
+        else if (Ball.position.y < 0) {
+            Debug.Log("Ball Pos " + Ball.position.y);
+            Ai.position = new Vector3(Ai.position.x, Ball.position.y -0.6f , 0);
+        }
+
+        if (pos.y > boundY) {
+            pos.y = boundY;
+        }
+        else if (pos.y < -boundY) {
+            pos.y = -boundY;
+        }
+        //transform.position = pos;
+    }
+}
+
